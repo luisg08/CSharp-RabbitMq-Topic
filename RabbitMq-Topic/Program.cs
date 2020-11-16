b1d418852a611915bff3ef68b7eccb57d6fb70ed
luisg08 <luisgust08@univem.edu.br>
2020-11-16 19:45:41 -0300

    Primeiro e Ultimo Commit

---
+++ RabbitMq-Topic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RequestRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            Topicmessages topicmessages = new Topicmessages();
+            do
+            {
+                Console.WriteLine("Enviar Mensagem?");
+                opt = Console.ReadLine();
+                if ((opt == "Sim") || (opt == "sim"))
+                    topicmessages.SendMessage();
+                else
+                    Environment.Exit(0);
+            } while (true);
+            
+        }
+    }
+}
---
+++ RabbitMq-Topic/Topicmessages.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+public class Topicmessages
+{
+    public void SendMessage()
+    {
+        //Conexão com o CloudAMQP
+        var connectionFactory = new ConnectionFactory();
+
+        connectionFactory.Uri = new Uri("amqps://ucrhweyl:eYCN6txHd4NPy_blBoKJIwKo1apxm3E5@jackal.rmq.cloudamqp.com/ucrhweyl");
+        connectionFactory.AutomaticRecoveryEnabled = true;
+
+        var connection = connectionFactory.CreateConnection();
+        var model = connection.CreateModel();
+        var properties = model.CreateBasicProperties();
+
+        properties.Persistent = false;
+
+        byte[] messagebuffer = Encoding.Default.GetBytes("Fila1");
+        model.BasicPublish("topic.exchange", "Menssagem.Fila1.test", properties, messagebuffer);
+        Console.WriteLine("RabbitMq Topic.");
+        Console.WriteLine("Mensagem enviada\n\n");
+    }
+}
